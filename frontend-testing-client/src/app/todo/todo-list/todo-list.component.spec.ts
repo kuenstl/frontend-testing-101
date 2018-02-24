d60d3baf63af1e2d1579e902156ef0b681c6b9ca
Michael <michael.p.steinberger@gmail.com>
2018-02-24 14:20:42 +0100

    Added unit tests for todo-list

--- frontend-testing-client/src/app/todo/todo-list/todo-list.component.spec.ts
+++ frontend-testing-client/src/app/todo/todo-list/todo-list.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, TestBed } from '@angular/core/testing';
+import { async, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -6,42 +6,73 @@ import { TodoItemComponent } from '../todo-item/todo-item.component';
 import { TodoListComponent } from './todo-list.component';
 import { TodoService } from '../todo.service';
 import { Todo } from '../todo.model';
+import { By } from '@angular/platform-browser';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/from';
 
 const firstTestTodo = new Todo('Write unit tests');
 const secondTestTodo = new Todo('Write more unit tests');
 
-class todoServiceStub {
-  getTodos() {
-    return [firstTestTodo, secondTestTodo];
-  }
-
-  addTodo(todo) {
-    return todo;
-  }
-
-  updateTodo(todo) {
-    return todo;
-  }
-
-  deleteTodo(todo) {}
-}
-
 describe('TodoListComponent', () => {
+  let fixture;
+  let component;
+  let todoService;
+  let addTodoSpy;
+
   beforeEach(
     async(() => {
+      const todoServiceStub = {
+        getTodos() {
+          return Observable.from([[firstTestTodo, secondTestTodo]]);
+        },
+
+        addTodo(todo) {
+          return Observable.from([todo]);
+        }
+      };
+
       TestBed.configureTestingModule({
         imports: [CommonModule, FormsModule],
-        providers: [{ provide: TodoService, useValue: todoServiceStub }],
-        declarations: [TodoItemComponent, TodoListComponent]
+        declarations: [TodoItemComponent, TodoListComponent],
+        providers: [{ provide: TodoService, useValue: todoServiceStub }]
       }).compileComponents();
+
+      fixture = TestBed.createComponent(TodoListComponent);
+      component = fixture.debugElement.componentInstance;
+      todoService = fixture.debugElement.injector.get(TodoService);
+
+      addTodoSpy = spyOn(todoService, 'addTodo').and.callThrough();
+      fixture.detectChanges();
     })
   );
+
   it(
     'should create the component',
     async(() => {
-      const fixture = TestBed.createComponent(TodoListComponent);
-      const component = fixture.debugElement.componentInstance;
       expect(component).toBeTruthy();
+    })
+  );
+
+  it(
+    'should show existing todos',
+    async(() => {
+      expect(component.todos).toContain(firstTestTodo);
+      expect(component.todos).toContain(secondTestTodo);
+    })
+  );
+
+  it(
+    'should add a todo',
+    fakeAsync(() => {
+      const newTestTodo = new Todo('Write another unit test');
+      component.newTodoText = newTestTodo.text;
+      fixture.detectChanges();
+      component.addTodo(newTestTodo);
+
+      fixture.detectChanges();
+      tick(10);
+      fixture.detectChanges();
+      expect(addTodoSpy).toHaveBeenCalledWith(newTestTodo);
     })
   );
 });
